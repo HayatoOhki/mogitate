9be9cdf7013312125f3816a393c7ff16d852ba14
ohki hayato <hayato1114.drums@gmail.com>
2024-09-11 12:31:01 +0900

    検索機能修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -22,18 +22,35 @@
 </div>
 <div class="main__content">
     <aside>
-        <form class="search__form" action="{{ route('products.search') }}" method="get">
-            <input type="text" name="keyword" placeholder="商品名で検索" value="{{ \Request::get('keyword') }}">
-            <button type="submit">検索</button>
-            <p>価格順で表示</p>
-            <div class="select__wrapper">
-                <select name="sort_order" id="sort_order" onchange="submit(this.form)">
-                    <option value="{{ \SortOrder::LIST['default'] }}" disabled @if(!\Request::get('sort_order')) selected @endif>価格で並べ替え</option>
-                    <option value="{{ \SortOrder::LIST['higherPrice'] }}"@if(\Request::get('sort_order') === \SortOrder::LIST['higherPrice']) selected @endif>高い順に表示</option>
-                    <option value="{{ \SortOrder::LIST['lowerPrice'] }}"@if(\Request::get('sort_order') === \SortOrder::LIST['lowerPrice']) selected @endif>低い順に表示</option>
-                </select>
-            </div>
-        </form>
+        <div class="search__area">
+            <form class="search__form" action="{{ route('products.search') }}" method="get">
+                <input type="text" name="keyword" placeholder="商品名で検索" value="{{ \Request::get('keyword') }}">
+                <button type="submit">検索</button>
+                <h3>価格順で表示</h3>
+                <div class="select__wrapper">
+                    <select name="sort_order" id="sort_order" onchange="submit(this.form)">
+                        <option value="{{ \SortOrder::LIST['default'] }}" disabled @if(!\Request::get('sort_order')) selected @endif>価格で並べ替え</option>
+                        <option value="{{ \SortOrder::LIST['higherPrice'] }}"@if(\Request::get('sort_order') === \SortOrder::LIST['higherPrice']) selected @endif>高い順に表示</option>
+                        <option value="{{ \SortOrder::LIST['lowerPrice'] }}"@if(\Request::get('sort_order') === \SortOrder::LIST['lowerPrice']) selected @endif>低い順に表示</option>
+                    </select>
+                </div>
+            </form>
+            @if(\Request::get('sort_order'))
+            <form class="tag__form" action="{{ route('products.search') }}" method="get">
+                @if(\Request::get('sort_order') === \SortOrder::LIST['higherPrice'])
+                <p>高い順に表示</p>
+                @elseif(\Request::get('sort_order') === \SortOrder::LIST['lowerPrice'])
+                <p>低い順に表示</p>
+                @endif
+                <input type="hidden" name="keyword" value="{{ \Request::get('keyword') }}">
+                @if(\Request::get('keyword'))
+                <button type="submit">✕</button>
+                @else
+                <a href="{{ route('products.index') }}">✕</a>
+                @endif
+            </form>
+            @endif
+        </div>
     </aside>
     <article>
         <div class="product__area">
